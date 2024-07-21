f92a48b55fd13dc999d4598b767f2f0b56eaa6c5
Cbitacritt3rs <139937587+Cbitacritt3rs@users.noreply.github.com>
2024-07-20 22:54:57 -0600

    Add files via upload

---
+++ trans.js
@@ -0,0 +1,24 @@
+function cambio2() {
+    document.getElementById('img1').classList.add('esconder');
+    document.getElementById('img2').classList.remove('esconder');
+}
+
+function cambio3() {
+    document.getElementById('img2').classList.add('esconder');
+    document.getElementById('img3').classList.remove('esconder');
+}
+
+function cambio4() {
+    document.getElementById('img2').classList.add('esconder');
+    document.getElementById('img4').classList.remove('esconder');
+}
+
+function regreso1() {
+    document.getElementById('img3').classList.add('esconder');
+    document.getElementById('img2').classList.remove('esconder');
+}
+
+function regreso2() {
+    document.getElementById('img4').classList.add('esconder');
+    document.getElementById('img2').classList.remove('esconder');
+}
